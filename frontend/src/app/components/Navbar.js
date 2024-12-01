931d2910785e86c03c2587efd8d9803a41c7dd37
Sahil Tiwari <162821589+tiwari02122003@users.noreply.github.com>
2024-12-01 18:27:48 +0530

    other update

--- frontend/src/app/components/Navbar.js
+++ frontend/src/app/components/Navbar.js
@@ -34,14 +34,14 @@ export function Navbar() {
           </div>
 
           
-          <div className="hidden md:flex">
+          {/* <div className="hidden md:flex">
             <Link
               href="#"
               className="px-4 py-2 border border-green-500 text-green-500 rounded-md hover:bg-green-500 hover:text-white transition-all font-mono"
             >
               CHOOSE PLAN
             </Link>
-          </div>
+          </div> */}
 
           
           <div className="md:hidden flex items-center">
